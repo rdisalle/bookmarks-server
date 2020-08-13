b33edbe8d4c5711a5c1fc73b8b5c5c29e5d94675
Ryan DiSalle <ryandisalle@yahoo.com>
2020-08-13 16:22:36 -0400

    updated

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -209,7 +209,7 @@ describe(`POST /api/bookmarks`, () => {
       })
     })
   })
-  describe.only(`PATCH /api/bookmarks:bookmark_id`, () => {
+  describe(`PATCH /api/bookmarks:bookmark_id`, () => {
     context(`Given no bookmarks`, () => {
       it(`responds with 404`, () => {
         const bookmarkId = 123456
